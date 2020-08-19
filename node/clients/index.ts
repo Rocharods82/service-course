179568c78b832368cd3e2a27ad4e7907d3dd02ab
Rodrigo Rocha <52714158+Rocharods82@users.noreply.github.com>
2020-08-19 01:47:49 -0300

    Configurações da chamada e clients.

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,7 +1,12 @@
 import { IOClients } from '@vtex/api'
+// @ts-ignore
+import Analytics from '../clients/analytics'
 
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
+    public get analytics() {
+        return this.getOrSet('analytics', Analytics)
+    }
 
 }
